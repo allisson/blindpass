02d542fc677c179c7116a0999a586244c44686fa
Allisson Azevedo <allisson@gmail.com>
2026-05-06 18:44:41 -0300

    fix(infra): increase webapp memory limit to 512Mi (#7)

--- terraform/modules/blindpass/main.tf
+++ terraform/modules/blindpass/main.tf
@@ -245,7 +245,7 @@ resource "google_cloud_run_v2_service" "webapp" {
       resources {
         limits = {
           cpu    = "1"
-          memory = "256Mi"
+          memory = "512Mi"
         }
       }
 
